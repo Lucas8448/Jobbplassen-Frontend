2022c8534677e5b432e35c963cf81a6d0f836e78
Alice <your_email@example.comgit config --global user.name Alice    git config --global user.email alice.bellander@gmail.com>
2025-10-31 11:21:08 +0100

    Forsøk på å lage knapp

--- src/app/komponenter/JobbBoks.tsx
+++ src/app/komponenter/JobbBoks.tsx
@@ -5,6 +5,7 @@ import {
   CardBlock,
   Heading,
   Paragraph,
+  Button
 } from "@digdir/designsystemet-react";
 import { useState } from 'react';
 
@@ -30,12 +31,31 @@ export default function JobbBoks({ id, title, content, employer, dato }: JobbBok
     setImageLoaded(true);
   };
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+      const response = await fetch(`/api/jobs/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+  };
+
   return (
     <div className="flex justify-center items-center mt-20">
       <Card
         data-color="neutral"
-        className="max-w-[650px] min-w-[650px] border-2 border-black mb-20"
+        className="max-w-[650px] min-w-[650px] border-2 border-black mb-20 relative"
       >
+        <div className="absolute top-2 right-2">
+          <Button 
+            type="submit"
+            className="bg-red-500 max-w-[80px] min-w-[80px] border-2 border-black bg-blue-600 text-black m-2 py-2 px-3 rounded-md cursor-pointer hover:text-gray-200"
+          >
+            Delete
+          </Button>
+        </div>
         <div className="flex flex-row">
           <CardBlock className="relative min-w-[120px] max-w-[170px] h-[250px] p-0 overflow-hidden bg-gray-100 flex items-center justify-center">
             {imageError ? (
